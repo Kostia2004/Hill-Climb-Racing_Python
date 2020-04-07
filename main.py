d57e9c67948c9c9f1986ab6f7597b724d862d5ba
Kostia2004 <K.A.Kudriavsev@mail.ru>
2020-04-07 22:10:34 +0300

    set frame rate has been added

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pygame
 import sys
 
-pygame.init() #initialization 
+pygame.init()                       #initialization 
  
 pygame.display.set_mode((600, 400)) #create window
+
+clock = pygame.time.Clock()         #clock object initialization
+
+FPS = 80                            #frame rate
 
 while True:                         #main program cycle
     for i in pygame.event.get():    #scrolling event list
         if i.type == pygame.QUIT:
             pygame.quit()           #program stop
             sys.exit()              #close window
+    clock.tick(FPS)                 #frame rate setting
