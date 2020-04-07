bb1c78df6e134a5f1c141891164333a74cf0ad65
Kostia2004 <K.A.Kudriavsev@mail.ru>
2020-04-07 16:20:20 +0300

    Main program cycle

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import pygame
+import sys
 
-pygame.init()
+pygame.init() #initialization 
  
-pygame.display.set_mode((600, 400))
+pygame.display.set_mode((600, 400)) #create window
+
+while True:                         #main program cycle
+    for i in pygame.event.get():    #scrolling event list
+        if i.type == pygame.QUIT:
+            pygame.quit()           #program stop
+            sys.exit()              #close window
