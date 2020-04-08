6b259db805ea7a894b42943a97a7b0828fcfdc2c
Kostia2004 <K.A.Kudriavsev@mail.ru>
2020-04-09 02:54:32 +0300

    added center line

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 import sys
+from tkinter import *
 
 pygame.init()                           #initialization 
 
 def main():   
-    pygame.display.set_mode((600, 400)) #create window
-
+    root = pygame.display.set_mode((700, 500)) #create window
+    pygame.display.set_caption("Pong")  #title of window
+    
     clock = pygame.time.Clock()         #clock object initialization
 
     FPS = 80                            #frame rate
@@ -15,6 +17,18 @@ def main():
             if i.type == pygame.QUIT:
                 pygame.quit()           #program stop
                 sys.exit()              #close window
+
+        #clear the screen to black: 
+        root.fill((0,0,0))
+        
+        #Draw the net:
+        pygame.draw.line(root, (255, 255, 255), [349, 0], [349, 500], 5)
+
+        # Go ahead and update the screen:
+        pygame.display.flip()
+
         clock.tick(FPS)                 #frame rate setting
+
+
 if __name__=="__main__":
     main()
