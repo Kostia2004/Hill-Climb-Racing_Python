162b5ff81f957919a0a0013cbde95b8ec4dc61cf
Kostia2004 <K.A.Kudriavsev@mail.ru>
2020-04-09 03:44:29 +0300

    bugs fixed racquets are shown

--- Racquet.py
+++ Racquet.py
@@ -2,11 +2,11 @@ from pygame import *
 
 class Racquet(sprite.Sprite):
 
-    def __init__(self, color, width, heigth):
+    def __init__(self, color, width, height):
         super().__init__()
 
         #background:
-        self.image = Surface([width, heigth])
+        self.image = Surface([width, height])
         self.image.fill((0,0,0))
         self.image.set_colorkey((0,0,0))
 
--- main.py
+++ main.py
@@ -7,6 +7,23 @@ pygame.init()                           #initialization
 def main():   
     root = pygame.display.set_mode((600, 400)) #create window
     pygame.display.set_caption("Pong")  #title of window
+
+    #first racket initialization:
+    RacquetA = Racquet((255, 255, 255), 10, 80)
+    RacquetA.rect.x = 20
+    RacquetA.rect.y = 160
+
+    #second racket initialization:
+    RacquetB = Racquet((255, 255, 255), 10, 80)
+    RacquetB.rect.x = 570
+    RacquetB.rect.y = 160
+
+    #list that will contain all the sprites to use in game:
+    all_sprites_list = pygame.sprite.Group()
+ 
+    # Add the paddles to the list of sprites
+    all_sprites_list.add(RacquetA)
+    all_sprites_list.add(RacquetB)
     
     clock = pygame.time.Clock()         #clock object initialization
 
@@ -23,6 +40,9 @@ def main():
         
         #Draw the net:
         pygame.draw.line(root, (255, 255, 255), [299, 0], [299, 400], 1)
+
+        #Draw game objects from the list of sprites
+        all_sprites_list.draw(root) 
 
         # Go ahead and update the screen:
         pygame.display.flip()
